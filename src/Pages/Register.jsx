6cf0f69b023b3ae6d814edea62ca25c28678e779
Shahriar262 <srh.2622@gmail.com>
2025-11-11 10:54:00 +0600

    added Register page with authentication

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,138 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router";
+import { AuthContext } from "../../context/AuthContext";
+import { FaGoogle } from "react-icons/fa6";
+import { toast } from "react-toastify";
+
 
 const Register = () => {
+     const { createUser, updateUserProfile, signInWithGoogle } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const displayName = event.target.displayName.value;
+    const photoURL = event.target.photoURL.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    toast.loading("Creating user...", { id: "create-user" });
+
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        addUserTodb({...result.user, displayName, photoURL})
+
+        updateUserProfile(displayName, photoURL)
+        toast.success("User created successfully!", { id: "create-user" });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message, { id: "create-user" });
+      });
+  };
+
+  const handleGoogleSignIn = () => {
+    toast.loading("Creating user...", { id: "create-user" });
+    signInWithGoogle()
+      .then((result) => {
+        toast.success("User created successfully!", { id: "create-user" });
+        addUserTodb(result.user);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.message || error.error, { id: "create-user" });
+      });
+  };
+
+  const addUserTodb = async (user) => {
+    const userData = {
+      uid: user.uid,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      email: user.email,
+    }
+    try {
+      const res = await fetch("https://m10-cs.vercel.app/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
     return (
-        <div>
-           Register page 
-        </div>
+       <div className="card bg-base-100 border border-gray-200 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-3xl font-bold text-center">Register</h1>
+        <form onSubmit={handleRegister}>
+          <fieldset className="fieldset">
+            {/* email field */}
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="displayName"
+              className="input rounded-full focus:border-0 focus:outline-gray-200"
+              placeholder="Name"
+            />
+
+            <label className="label">PhotoURL</label>
+            <input
+              type="text"
+              name="photoURL"
+              className="input rounded-full focus:border-0 focus:outline-gray-200"
+              placeholder="Photo URL"
+            />
+            {/* email field */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input rounded-full focus:border-0 focus:outline-gray-200"
+              placeholder="Email"
+            />
+            {/* password field */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input rounded-full focus:border-0 focus:outline-gray-200"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn text-white mt-4 rounded-full bg-linear-to-r from-pink-500 to-red-600">
+              Register
+            </button>
+          </fieldset>
+        </form>
+
+        <button
+          onClick={handleGoogleSignIn}
+          className="btn bg-white rounded-full text-black border-[#e5e5e5]"
+        >
+          <FaGoogle />
+          Login with Google
+        </button>
+        <p className="text-center">
+          Already have an account? Please{" "}
+          <Link className="text-blue-500 hover:text-blue-800" to="/auth/login">
+            Login
+          </Link>{" "}
+        </p>
+      </div>
+    </div>
     );
 };
 
