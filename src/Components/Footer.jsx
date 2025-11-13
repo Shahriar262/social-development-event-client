8cd73f58bddc94c2c721e1c143fabc6b5cc07810
Shahriar262 <srh.2622@gmail.com>
2025-11-13 18:37:47 +0600

    fix errors

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
-import { FaX, FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import logoImg from "../assets/logo.png";
 
 const Footer = () => {
   return (
     <footer className="bg-slate-800 text-gray-200 py-10 mt-20">
       <div className="max-w-7xl mx-auto px-4 md:px-0 grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* About Section */}
+        
         <div>
-          <h2 className="text-2xl font-bold mb-3 text-white">ServeBangla</h2>
+          <div className="flex items-center gap-2">
+            <img
+            src={logoImg}
+            className="w-7 h-7 rounded-full mb-3"
+            alt=""
+          />
+          <h2 className="text-2xl font-bold mb-3 text-white">Serve<span className="text-purple-400">Bangla</span></h2>
+          </div>
           <p className="text-gray-400">
             ServeBangla is a community-driven platform to create, join, and
             track social development events across Bangladesh. Join hands for a
